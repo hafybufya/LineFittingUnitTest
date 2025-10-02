c454eec7055e548b1fe0f80c0ea981cc5f23dfce
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-10-02 15:02:11 +0100

    included the correct the slope of the line and intercept

--- lineCode.py
+++ lineCode.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 
 # Load CSV data (remove spaces in your CSV file first)
 data = np.loadtxt('lineEquation.csv', delimiter=',')
-print(data)
 
 def equation_of_line(x_value, y_value):
-    slope_m = 1
-    intercept_c = 0
+    slope_m = 1.85
+    intercept_c = 1.22
     # Example: y = mx + c
     
     return slope_m * x_value + intercept_c
