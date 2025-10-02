9b14ff07134be32d584af44446e61120ba6422d6
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-10-02 14:53:10 +0100

    fixed the csv to have more scatter

--- lineCode.py
+++ lineCode.py
@@ -1,8 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt('lineEquation.csv', delimitter=',', skiprows =1)
+# Load CSV data (remove spaces in your CSV file first)
+data = np.loadtxt('lineEquation.csv', delimiter=',')
 print(data)
-def equation_of_line( x_value, y_value):
+
+def equation_of_line(x_value, y_value):
     slope_m = 1
     intercept_c = 0
+    # Example: y = mx + c
+    
+    return slope_m * x_value + intercept_c
+
+# Example usage
+x = data[0]
+y = data[1]
+plt.plot(x, y, 'o')
+plt.show()
