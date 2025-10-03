d39fa6e9746ced21fc18617086ab60c5253439a9
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-10-03 11:06:15 +0100

    Fixed the code so not i have an operating graph. Made a unitTest.py i will popualte laeter

--- lineCode.py
+++ lineCode.py
@@ -1,29 +1,23 @@
-import numpy as np
+
 import matplotlib.pyplot as plt
 import pandas as pd
-#
-#from scipy.stats import linregress
+from scipy.stats import linregress
 # Load CSV data (remove spaces in your CSV file first)
 
-lineCode_pandas = pd.read_csv('lineCode_pandas.csv')
-x = lineCode_pandas['x'] #y working hours
-y = lineCode_pandas['y'] #y axis life expectancy
+lineCode_pandas = pd.read_csv('lineCsvFile.csv')
+# Extract x and y
+x = lineCode_pandas['x']
+y = lineCode_pandas['y']
+
+slope, intercept, r_value, p_value, std_err = linregress(x, y)
+
+plt.plot(x, slope*x + intercept, color='r',
+         label=f"Regression line\ny = {slope:.2f}x + {intercept:.2f}")
+
+plt.scatter(x, y, label="Data Points")
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.legend()
+plt.show()
 
 
-#regression stats stuff
-#slope, intercept, r_value, p_value, std_err = linregress(x, y)
-
-# fig, ax = plt.subplots()
-# ax.scatter(x, y) #scatters total and divorces
-
-# ax.set_xlabel('Average annual working hours per worker', fontsize=10)  
-# ax.set_ylabel('Life expectancy at birth', fontsize=10)
-# ax.plot(x, slope*x + intercept, color='r', label='Regression line \n y=-0.01x + 96.61')  #regression line
-# ax.legend()
-# ax.set_title('Working hours vs Life Expectancy')
-
-# #everything here used to make regression line
-# print(f" slope = {slope}, intercept = {intercept}")
-# print(f" Pearson correlation = {r_value}, and p-value = {p_value}")
-
-# #splitting the graph up because it looks like two different correlations are being shown rn dun dun dunnnnnn
